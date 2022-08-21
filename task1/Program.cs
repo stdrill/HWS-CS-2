d53f77464b27c609a29fc1b47c9737df4b1f5e97
stdrill <tanya23.96@mail.ru>
2022-08-21 15:06:16 +0300

    Task 1 added

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+string[] arrayWeek = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+Console.WriteLine("Введите порядковый номер: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= arrayWeek.Length && n > 0){
+    if (n <=5){
+    Console.WriteLine("{0} будний день", arrayWeek[n - 1]);}
+    if (n > 5 && n <= arrayWeek.Length){
+        Console.WriteLine("{0} выходной день!", arrayWeek[n - 1]);
+    }
+}else {
+    Console.WriteLine("Введеное число не совпадает с днем недели");
+}
