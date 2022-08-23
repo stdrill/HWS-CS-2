0ced56a12e36518ffb16be9fcf4e009cbfd5dd2f
stdrill <tanya23.96@mail.ru>
2022-08-23 21:57:58 +0300

    Task 3 added

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1){
+    Console.WriteLine("[10, 10]");
+} else if (a == 2){
+    Console.WriteLine("[-10, 10]");
+} else if (a == 3) {
+    Console.WriteLine("[-10, -10]");
+} else if (a == 4) {
+    Console.WriteLine("[10, -10]");
+} 
